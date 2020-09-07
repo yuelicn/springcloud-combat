f7fe2071e834912388322d06c702020da0e42eca
yueli <yueli@tusdao.com>
2020-09-07 15:39:12 +0800

    注册中心Eureka-server

---
+++ springcloud-eureka/src/main/java/com/springcloud/eureka/Application.java
@@ -0,0 +1,17 @@
+package com.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yueli
+ * @date 2020/9/7 14:45
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ springcloud-eureka/src/main/java/com/springcloud/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.springcloud.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author yueli
+ * @date 2020-09-07 14:51
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
