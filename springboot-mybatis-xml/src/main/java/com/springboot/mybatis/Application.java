a8e9c2e2c3f68f25a2f1de82b9b767e9ea0a61b5
yueli <yueli@tusdao.com>
2020-09-14 14:54:37 +0800

    添加mybatis的配置

--- springboot-mybatis-xml/src/main/java/com/springboot/mybatis/Application.java
+++ springboot-mybatis-xml/src/main/java/com/springboot/mybatis/Application.java
@@ -1,5 +1,6 @@
 package com.springboot.mybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/9/14 13:28
  */
 @SpringBootApplication
+@MapperScan("com.springboot.mybatis.dao")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
